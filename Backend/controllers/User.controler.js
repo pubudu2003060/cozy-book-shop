fe56339f895dfba378f5fcda07e6f420e345cbf8
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-08-22 18:06:05 +0530

    changes in user controler

--- Backend/controllers/User.controler.js
+++ Backend/controllers/User.controler.js
@@ -4,7 +4,7 @@ export const isLoged = async (req, res) => {
       .status(200)
       .json({ success: true, message: "This user is authenticaed" });
   } catch (error) {
-    ress
+    res
       .status(500)
       .json({ success: false, message: "This user is not authenticaed" });
   }
