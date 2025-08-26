aee8b26a0e282190d54902bc3b270387b7d86da4
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-08-27 02:19:48 +0530

    feat: Update server start script and add API routes in index.js

---
+++ Backend/api/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDb } from "./configs/DBConnection.js";
+import bookRoute from "./rotues/Book.route.js";
+import cartRoute from "./rotues/Cart.route.js";
+import emailRouter from "./rotues/Email.route.js";
+import userRoute from "./rotues/User.route.js";
+import authRouter from "./rotues/Auth.route.js";
+import startHTTPSServer from "./configs/HttpsServer.js";
+import orderRoute from "./rotues/Order.route.js";
+import Book from "./models/Book.model.js";
+
+const app = express();
+
+dotenv.config();
+
+app.use(
+  cors({
+    origin: ["https://localhost:5173", "https://cozybookshop.netlify.app"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.use("/api/auth", authRouter);
+
+app.use("/api/book", bookRoute);
+
+app.use("/api/cart", cartRoute);
+
+app.use("/api/email", emailRouter);
+
+app.use("/api/user", userRoute);
+
+app.use("/api/order", orderRoute);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status).json({ error: err.message });
+});
+
+export default app;
