2f024622482c45a02c2425e8b545d8c2d141e901
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-09-07 13:09:36 +0530

    change ceets paths in backend

--- Backend/configs/HttpsServer.js
+++ Backend/configs/HttpsServer.js
@@ -2,8 +2,14 @@ import https from "https";
 import fs from "fs";
 import path from "path";
 
-const privateKey = fs.readFileSync(path.join("", "localhost-key.pem"), "utf8");
-const certificate = fs.readFileSync(path.join("", "localhost.pem"), "utf8");
+const privateKey = fs.readFileSync(
+  path.join("", "certs/localhost-key.pem"),
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  path.join("", "certs/localhost.pem"),
+  "utf8"
+);
 const credentials = { key: privateKey, cert: certificate };
 
 const startHTTPSServer = (app, port, database) => {
